42fc39e78431ad7f00fbd0d81bdee0ab45ad2a89
Alumno <Alumno@LAB1507-21>
2023-06-13 14:10:29 -0500

    segundo commit

--- src/main/java/com/tecsup/petclinic/services/PetServiceImpl.java
+++ src/main/java/com/tecsup/petclinic/services/PetServiceImpl.java
@@ -2,10 +2,8 @@ package com.tecsup.petclinic.services;
 
 import java.util.List;
 import java.util.Optional;
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
 import com.tecsup.petclinic.entities.Pet;
 import com.tecsup.petclinic.exception.PetNotFoundException;
 import com.tecsup.petclinic.repositories.PetRepository;
